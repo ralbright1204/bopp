44efab68379fde3abd624f04b5a080e98b474b45
reggieroby <reggie.a.roby@gmail.com>
2015-05-02 16:06:58 -0500

    state router enabled

--- app/ng/boppApp.js
+++ app/ng/boppApp.js
@@ -1,1 +1,24 @@
-var app = angular.module("boppApp", ["leaflet-directive", "ui.router"]);
+var app = angular.module("boppApp", ["leaflet-directive", "ui.router"])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('landing', {
+      url: "/",
+      templateUrl: "app/pages/landing.html"
+    })
+    .state('map', {
+      url: "/map",
+      templateUrl: "app/pages/map.html",
+      controller: "MapController"
+    })
+    .state('twitter', {
+      url: "/twitter",
+      templateUrl: "app/pages/timeline.html",
+      controller: "twitterCtrl"
+    });
+});
