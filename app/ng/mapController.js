7b704374ae7f730165074544de96a1ed14fa5c5a
Bradley Dice <bdice@bradleydice.com>
2015-05-03 10:34:15 -0500

    Removed test markers.

--- app/ng/mapController.js
+++ app/ng/mapController.js
@@ -41,22 +41,6 @@ app.controller("MapController", [ '$scope', '$http', function($scope, $http) {
             lng: -94.5822,
             zoom: 13
         },
-        markers: {
-            testMarker1: {
-                lat: 39.0978,
-                lng: -94.5822,
-                message: "OMG PIZZA!",
-                focus: true,
-                draggable: false
-            },
-            testMarker2: {
-                lat: 39.0980,
-                lng: -94.5820,
-                message: "Fresh jazz!",
-                focus: true,
-                draggable: false
-            }
-        },
         layers: {
             baselayers: {
                 xyz: {
