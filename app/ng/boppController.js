c3327eb4ca4e74abca2e6ea9fb677353341834a6
Bradley Dice <bdice@bradleydice.com>
2015-05-03 13:34:36 -0500

    Menu state fixed?

--- app/ng/boppController.js
+++ app/ng/boppController.js
@@ -1,4 +1,4 @@
-app.controller('BoppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+app.controller('BoppController', ['$scope', '$mdSidenav', '$state', function($scope, $mdSidenav, $state) {
   
   $scope.selected = null;
   $scope.toggleSidenav = toggleSidenav;
